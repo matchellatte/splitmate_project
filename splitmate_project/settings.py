ea822d29aa2b29ed943139f07f4f1f3a7fb2fbdc
cyberchelle <grechelleboneo97@gmail.com>
2025-05-11 00:47:50 +0800

    modified:   splitmate_project/settings.py

--- splitmate_project/settings.py
+++ splitmate_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m&7pwnsmy08odfr3bgyp+*p#d2ado@%(!noyj8@egeg2^blktx'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']  # Update this with your actual domain later
 
@@ -142,9 +143,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # or your provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gglassdump@gmail.com'
-EMAIL_HOST_PASSWORD = 'mrop dook talm jqhc'
-DEFAULT_FROM_EMAIL = "SplitMate <gglassdump@gmail.com>"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = f"SplitMate <{os.environ.get('EMAIL_HOST_USER', '')}>"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
